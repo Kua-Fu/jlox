1ac10295c430339a7ec00bf73b583744d738c087
yz <1126978661@qq.com>
2022-09-20 15:40:46 +0800

    update code

--- app/src/test/java/com/craftinginterpreters/lox/AppTest.java
+++ app/src/test/java/com/craftinginterpreters/lox/AppTest.java
@@ -3,12 +3,12 @@
  */
 package com.craftinginterpreters.lox;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+// import org.junit.jupiter.api.Test;
+// import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
-//    @Test void appHasAGreeting() {
-//        App classUnderTest = new App();
-//        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
-//    }
+    // @Test void appHasAGreeting() {
+    // App classUnderTest = new App();
+    // assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+    // }
 }
