2bd57e8f9578734f3626b3cd401cde30fa853130
yz <1126978661@qq.com>
2022-11-12 11:19:54 +0800

    update code, about new parse

--- app/src/main/java/com/craftinginterpreters/lox/App.java
+++ app/src/main/java/com/craftinginterpreters/lox/App.java
@@ -53,9 +53,15 @@ public class App {
         Scanner scanner = new Scanner(source);
         List<Token> tokens = scanner.scanTokens();
 
-        for (Token token : tokens) {
-            System.out.println(token);
-        }
+        // for (Token token : tokens) {
+        // System.out.println(token);
+        // }
+        Parser parser = new Parser(tokens);
+        Expr expression = parser.parse();
+
+        if (hadError)
+            return;
+        System.out.println(new AstPrinter().print(expression));
     }
 
     static void error(int line, String message) {
@@ -65,5 +71,13 @@ public class App {
     private static void report(int line, String where, String message) {
         System.err.println("[line " + line + "] Error" + where + ": " + message);
         hadError = true;
+    }
+
+    static void error(Token token, String message) {
+        if (token.type == TokenType.EOF) {
+            report(token.line, " at end ", message);
+        } else {
+            report(token.line, " at " + token.lexeme + "'", message);
+        }
     }
 }
